03d8a3cccc6072975ca3888b73c3a61515b9a5bf
KQuyen123 <24521497@gm.uit.edu.vn>
2026-05-08 09:39:10 +0700

    update student feature

--- student.py
+++
@@ -1,8 +0,0 @@
-def add_student(name, student_id, gpa):
-    student = {
-        "name": name,
-        "id": student_id,
-        "gpa": gpa
-    }
-    students.append(student)
-    print(f"Đã thêm sinh viên: {name}")

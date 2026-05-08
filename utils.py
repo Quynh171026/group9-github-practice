f1fd9a90fe4cb8e3ff30386313520a192c6ef370
TrinhHuong23 <24520647@gm.uit.edu.vn>
2026-05-08 09:00:49 +0700

    Add functions to manage student records

--- utils.py
+++ utils.py
@@ -1,1 +1,53 @@
+from student import students
 
+def add_student():
+    name = input("Nhập tên sinh viên: ")
+    students.append(name)
+    print("Đã thêm sinh viên!")
+
+def show_students():
+    if len(students) == 0:
+        print("Danh sách trống!")
+    else:
+        print("\n===== DANH SÁCH SINH VIÊN =====")
+        for i, student in enumerate(students, start=1):
+            print(f"{i}. {student}")
+
+def find_student():
+    keyword = input("Nhập tên cần tìm: ")
+
+    found = False
+
+    for student in students:
+        if keyword.lower() in student.lower():
+            print("Tìm thấy:", student)
+            found = True
+
+    if not found:
+        print("Không tìm thấy sinh viên!")from student import students
+
+def add_student():
+    name = input("Nhập tên sinh viên: ")
+    students.append(name)
+    print("Đã thêm sinh viên!")
+
+def show_students():
+    if len(students) == 0:
+        print("Danh sách trống!")
+    else:
+        print("\n===== DANH SÁCH SINH VIÊN =====")
+        for i, student in enumerate(students, start=1):
+            print(f"{i}. {student}")
+
+def find_student():
+    keyword = input("Nhập tên cần tìm: ")
+
+    found = False
+
+    for student in students:
+        if keyword.lower() in student.lower():
+            print("Tìm thấy:", student)
+            found = True
+
+    if not found:
+        print("Không tìm thấy sinh viên!")

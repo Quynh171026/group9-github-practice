2374d4f95d4fc6155a2dbd17884b267abfa5f5d3
Quynh171026 <24520471@gm.uit.edu.vn>
2026-05-08 09:18:33 +0700

    Add search student function

--- student.py
+++ student.py
@@ -1,1 +1,8 @@
+def search_student(students, keyword):
+    result = []
 
+    for student in students:
+        if keyword.lower() in student.lower():
+            result.append(student)
+
+    return result

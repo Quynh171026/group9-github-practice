aae0351506f35a838c35960dc19bd609ac4ec6e9
Quynh171026 <24520471@gm.uit.edu.vn>
2026-05-08 09:42:23 +0700

    Clear student file

--- student.py
+++
@@ -1,8 +0,0 @@
-def search_student(students, keyword):
-    result = []
-
-    for student in students:
-        if keyword.lower() in student.lower():
-            result.append(student)
-
-    return result

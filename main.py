c24dc547fe06ca0ac82d4917ffe0312171e0e6cf
NguyenThiQuynh <nguyenthiquynh10a01@gmail.com>
2026-05-08 09:11:22 +0700

    Add menu

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from utils import add_student, show_students, find_student
 
+def menu():
+    while True:
+        print("\n===== MENU =====")
+        print("1. Thêm sinh viên")
+        print("2. Hiển thị danh sách")
+        print("3. Tìm kiếm sinh viên")
+        print("0. Thoát")
+
+        choice = input("Chọn chức năng: ")
+
+        if choice == "1":
+            add_student()
+        elif choice == "2":
+            show_students()
+        elif choice == "3":
+            find_student()
+        elif choice == "0":
+            print("Thoát chương trình!")
+            break
+        else:
+            print("Lựa chọn không hợp lệ!")
+
+if __name__ == "__main__":
+    menu()

5c5adb8c0be64e094350d6abd38a45001c91b417
NguyenThiQuynh <nguyenthiquynh10a01@gmail.com>
2026-05-08 09:42:55 +0700

    Add utils functions

--- utils.py
+++ utils.py
@@ -2,25 +2,8 @@ from student import Student
 
 students = []
 
-def add_student():
-    id = input("Nhập ID: ")
-    name = input("Nhập tên: ")
-    students.append(Student(id, name))
-    print("Đã thêm!")
+def add_student()
 
-def show_students():
-    if not students:
-        print("Danh sách rỗng!")
-        return
-    for s in students:
-        print(s)
+def show_students()
 
-def find_student():
-    keyword = input("Nhập tên cần tìm: ")
-    found = False
-    for s in students:
-        if keyword.lower() in s.name.lower():
-            print(s)
-            found = True
-    if not found:
-        print("Không tìm thấy!")
+def find_student()

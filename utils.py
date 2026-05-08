5da76548b0bca2974a097f3539e5891746d01c12
Hương <24520647@gm.uit.edu.vn>
2026-05-08 09:33:11 +0700

    Hoàn thiện chức năng hiển thị danh sách sinh viên

--- utils.py
+++ utils.py
@@ -1,1 +1,11 @@
+def show_students():
+    print("\n===== DANH SÁCH SINH VIÊN =====")
+
+    if len(students) == 0:
+        print("Hiện chưa có sinh viên nào.")
+    else:
+        for i, student in enumerate(students, start=1):
+            print(f"STT {i}: {student}")
+
+        print(f"\nTổng số sinh viên: {len(students)}")
 
